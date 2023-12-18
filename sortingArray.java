2a8654342c96211aed9f0b705b24585fe9bf28e2
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-12-18 17:54:45 +0530

    Create sortingArray.java

---
+++ sortingArray.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static boolean isSorted(int arr[],int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+        return isSorted(arr,i+1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,1,3,4};
+        System.out.println(isSorted(arr,0));
+    }
+}
